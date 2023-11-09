cb00ed29c3ccebd93d0316f5fabde56ad2e4d86f
a512154224 <512154224@qq.com>
2023-11-09 17:07:45 +0800

    Update nwct_xmq.py
    1、超过5分钟切换备用下载链接

--- nwct_xmq.py
+++ nwct_xmq.py
@@ -22,11 +22,14 @@ import requests
 from time import sleep
 
 def check_os():
+    global url
     print('正在检测设备架构.')
     cpu_arch = os.popen('uname -m').read()
     if 'arm' in cpu_arch or 'aarch64' in cpu_arch:
+        url = "http://note.youdao.com/yws/api/personal/file/WEB59a6cc544058b069c139957f720ad471?method=download&inline=true&shareKey=06b9e878c8991524296fda31791826fa"
         download_app('arm')
     elif 'x86' in cpu_arch or 'x64' in cpu_arch:
+        url = "http://note.youdao.com/yws/api/personal/file/WEB2631b2592aaa8a72e4649c9b96653208?method=download&inline=true&shareKey=f66ac55dba68dbe929028ff6c231758d"
         download_app('386')
     else:
         print('暂不支持当前架构.')
@@ -37,14 +40,22 @@ def download_app(cpu):
         print("正在检测穿透程序.\n尝试下载穿透程序.")
         try:
             app_url = f"https://github.com/a512154224/qinglongscripts/raw/main/xiaomiqiu_{cpu}"
-            res = requests.get(app_url)
+            res = requests.get(app_url, timeout=300)
             with open("xiaomiqiu", "wb") as f:
                 f.write(res.content)
             print("穿透程序下载成功.")
             start_nwct()
         except:
-            print("穿透程序下载失败.\n请检查网络后重试.")
-            os.system(f"rm -rf {app_path}")
+            print("穿透程序下载失败.\n切换下载地址重试.")
+            try:
+                res = requests.get(url, timeout=300)
+                with open("xiaomiqiu", "wb") as f:
+                    f.write(res.content)
+                print("穿透程序下载成功.")
+                start_nwct()
+            except:
+                print("穿透程序下载失败.\n请检查网络后重试.")
+                os.system(f"rm -rf {app_path}")
     else:
         print("检测穿透程序存在.")
         start_nwct()
